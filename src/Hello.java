4ed19af1344a1a6b51cd0840d28d9f5fec8ab881
raducan andrei <raducan.andrei@yahoo.com>
2018-11-09 19:14:52 +0200

    import

---
+++ src/Hello.java
@@ -0,0 +1,12 @@
+public class Hello {
+    public static void main(String[] args) {
+        /*printam ceva:
+         */
+
+
+
+        System.out.print("Hello world");
+        System.out.print("Hello world");
+
+    }
+}
